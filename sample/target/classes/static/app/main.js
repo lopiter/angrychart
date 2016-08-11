4e43c75157f908275dddb0db7387452c3e210dce
kiseok.jang <wkdrltjr#1>
2016-08-11 16:28:18 +0900

    delete target

--- sample/target/classes/static/app/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-const app_sample_1 = require("./app.sample");
-platform_browser_dynamic_1.bootstrap(app_sample_1.SampleComponent);
